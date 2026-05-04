ffbe7932aaeb4c81226a8fcff8dc9e1d8760c82f
John Rogee Turqueza <jrogeet@gmail.com>
2026-05-05 07:54:25 +0800

    Add database models and setup for story management
    - Introduced database connection and session management in `database.py`.
    - Created `StoryJob` model in `job.py` to track story job progress.
    - Developed `Story` and `StoryNode` models in `story.py` to represent stories and their nodes with relationships.

---
+++ backend/models/job.py
@@ -0,0 +1,19 @@
+# Represents the Intent to make a story and has a progress, like In Progress, Finished, etc.
+
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+
+from db.database import Base
+
+class StoryJob(Base):
+    __tablename__ = "story_jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_id = Column(String, index=True, unique=True)
+    session_id = Column(String, index=True)
+    theme = Column(String)
+    status = Column(String)
+    story_id = Column(Integer, nullable=True) # nullable=True means it can have Null or No Value
+    error = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    completed_job = Column(DateTime(timezone=True), nullable=True)
---
+++ backend/models/story.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+from db.database import Base
+
+class Story():
+    __tablename__ = "stories"
+
+    # just like how we query in SQL:
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    # to see stories created by a browser session:
+    session_id = Column(String, index=True)
+    # automatically grab current time:
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    # one-to-many relationship, multiple nodes connected to 1 story
+    nodes = relationship("StoryNode", back_populates="story")
+
+# Different nodes or paths per story
+class StoryNode(Base):
+    __tablename__ = "story_nodes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    story_id = Column(Integer, ForeignKey("stories.id"), index=True)
+    content = Column(String)
+    is_root = Column(Boolean, default=False)
+    is_ending = Column(Boolean, default=False)
+    is_winning_ending = Column(Boolean, default=False)
+    options = Column(JSON, default=list)
+
+    story = relationship("Story", back_populates="nodes")
